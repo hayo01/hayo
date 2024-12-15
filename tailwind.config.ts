8e76bf9553e324a828e2df710dd4c7103306c435
hayo <yhao9172@gmail.com>
2024-12-15 23:53:30 +0900

    add keyframes animation config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,24 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+      },
+      keyframes: {
+        blink: {
+          to: { opacity: '0' },
+        },
+      },
+      animation: {
+        typing: 'blink 1.5s steps(2) infinite',
       },
     },
   },
