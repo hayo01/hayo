ec1ff4a00e67bee8a2ede07b29aba73defb45f8b
hayo <yhao9172@gmail.com>
2024-12-15 23:54:59 +0900

    done first page UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,26 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="">
-      <div className="">
-        <p>Profile</p>
+    <div className="h-screen flex flex-col p-20 font-mono">
+      <div className="flex justify-center pb-10">
+        <p className="text-2xl font-extrabold">Hayoung Yang</p>
+      </div>
+      <div className="pb-8">
+        <p className="text-xl font-bold">Skills</p>
+        <p className="text-lg font-medium">
+          - Korean, English
+          <br />- JavaScript, TypeScript, React, Next.js, Java, etc.
+          <br />- MySQL, MariaDB
+          <br />- Git, Confluence, Jira, Discord, Teams, Notion, etc.
+        </p>
       </div>
       <div className="">
-        <p>Experience</p>
+        <p className="text-xl font-bold">What you can check</p>
+        <Link href={'/calculator'} className="text-lg font-semibold underline">
+          ▶ Calculator
+        </Link>
       </div>
-      <div className="">
-        <Link href={'/calculator'}>Calculator</Link>
-      </div>
+      <p className="text-lg animate-typing">▌</p>
     </div>
   );
 }
