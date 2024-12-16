d7b59ad46486e077780122c4292a3220ce8c9235
hayo <yhao9172@gmail.com>
2024-12-16 20:48:17 +0900

    update UI

--- src/app/calculator/_components/Button.tsx
+++ src/app/calculator/_components/Button.tsx
@@ -10,7 +10,7 @@ interface BtnProps {
 export default function Button(p: BtnProps) {
   return (
     <button
-      className={`${p.style} flex justify-center content-center border rounded-full p-1.5 `}
+      className={`${p.style} flex justify-center content-center border rounded-md p-1.5 text-base font-bold hover:scale-125 hover:text-[#0a1d0b] hover:bg-[#5ee8b3]`}
       onClick={p.onClick}
     >
       {p.children}
